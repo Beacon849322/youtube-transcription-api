6789f971cef539e743d0e7d96d564c8d88496390
Beacon849322 <beacon@sju.edu.in>
2025-03-06 18:24:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,6 @@ def transcribe():
         return jsonify({"transcript": text})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get("PORT", 10000))
+app.run(host='0.0.0.0', port=port)
