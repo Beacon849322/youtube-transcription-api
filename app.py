ebe1d35c7fc54d4cf9f2ffb32605ef4b7287e3db
Beacon849322 <beacon@sju.edu.in>
2025-03-07 11:57:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from youtube_transcript_api import YouTubeTranscriptApi
 
 app = Flask(__name__)
 
-COLAB_TRANSCRIBE_URL = "https://91a2-34-19-86-197.ngrok-free.app"
+COLAB_TRANSCRIBE_URL = "https://a40b-34-19-86-197.ngrok-free.app"
 
 @app.route('/transcribe', methods=['GET'])
 def transcribe():
